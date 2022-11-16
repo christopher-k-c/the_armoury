adde5cbcd1d53425ed578ea4e6e3bc223b4868ec
christopher-k-c <chriscareyk@gmail.com>
2022-11-16 17:54:06 +0000

    Eighth Commit, General tweaks & Modal content animation

--- main.js
+++ main.js
@@ -6,15 +6,11 @@ tl.from('svg', {scale: 0.5, duration: 1, opacity: 0})
  .to('.reveal', {clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', y:0, stagger: .3}, "-=2.9")
 
 
-
-
-
-
-
 // Display and Hide the modal
 const modal = (event) =>{
 
-    const tl2 = gsap.timeline()
+    let tl1 = gsap.timeline()
+    
 
     const modalContainer = document.getElementById('modal');
     const newDiv = document.createElement('div');
@@ -22,15 +18,15 @@ const modal = (event) =>{
     newDiv.setAttribute('onclick', 'modal(event.target)')
     const getNewDiv = document.getElementById('darken')
     
-
-
-
     if('none' == modalContainer.style.display){
-        tl2.from('#modal', {duration: 1, x: '100%'})
+        tl1.from('#modal', {duration: 1, x: '100%'})
+         .from('.initial-navaigation', {duration: 0.8, opacity: 0})
+         .from('.secondary-nav', {duration: 0.8, opacity: 0})
+         .from('.collection', {duration: 0.8, opacity: 0})
+      
         modalContainer.style.display = 'block';
         document.body.appendChild(newDiv)
     } else if(event == getNewDiv){
-
 
         getNewDiv.remove();
         modalContainer.style.display = 'none'
@@ -41,3 +37,12 @@ const modal = (event) =>{
     }
 
 }
+
+
+    
+
+
+
+
+
+
